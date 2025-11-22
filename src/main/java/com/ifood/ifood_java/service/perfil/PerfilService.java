543f23796ad97ad8343fd8f6b52244e2eeebbb9b
karlos <karlosmarques@gmail.com>
2025-11-22 19:05:38 -0300

    service de perfil

---
+++ src/main/java/com/ifood/ifood_java/service/perfil/PerfilService.java
@@ -0,0 +1,69 @@
+package com.ifood.ifood_java.service.perfil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.ifood.ifood_java.controller.perfil.AtualizarPerfilRequest;
+import com.ifood.ifood_java.entity.usuario.Usuario;
+import com.ifood.ifood_java.entity.usuario.UsuarioRequest;
+import com.ifood.ifood_java.repository.UsuarioRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class PerfilService {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioRequest pegarPerfil() {
+       
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new RuntimeException("Usuário não autenticado");
+        }
+        String email = authentication.getName();
+
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        return new UsuarioRequest(
+            usuario.getNome(),
+            usuario.getNome(),
+            usuario.getEmail(),
+            usuario.getCpf(),
+            usuario.getDtNascimento(),
+            usuario.getFoneCelular()
+        );
+    }
+    @Transactional
+    public AtualizarPerfilRequest atualizarPerfil(AtualizarPerfilRequest request) {
+    
+    Usuario usuario = usuarioRepository
+            .findByEmail(SecurityContextHolder.getContext()
+                    .getAuthentication()
+                    .getName())
+            .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+    if (request.getNome() != null && !request.getNome().isBlank()) {
+        usuario.setNome(request.getNome());
+    }
+    if (request.getFoneCelular() != null) {
+        usuario.setFoneCelular(request.getFoneCelular());
+    }
+    if (request.getDtNascimento() != null) {
+        usuario.setDtNascimento(request.getDtNascimento());
+    }
+
+    Usuario atualizado = usuarioRepository.save(usuario);
+    
+    return new AtualizarPerfilRequest(
+        atualizado.getNome(),
+        atualizado.getDtNascimento(),
+        atualizado.getFoneCelular()
+    );
+}
+}
