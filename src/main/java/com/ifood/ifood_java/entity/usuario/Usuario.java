0250cf6d6c04438acb1d33220f1bc4093050b0b3
karlos <karlosmarques@gmail.com>
2025-11-22 19:04:56 -0300

    entity perfil

--- src/main/java/com/ifood/ifood_java/entity/usuario/Usuario.java
+++ src/main/java/com/ifood/ifood_java/entity/usuario/Usuario.java
@@ -2,41 +2,45 @@ package com.ifood.ifood_java.entity.usuario;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Table(name = "usuarios" )
+@Table(name = "usuarios")
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
-
 public class Usuario {
+   
    @Id
-   @GeneratedValue(strategy =GenerationType.IDENTITY )
-   private Long id_usuario;
-
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id_usuario")
+   private Long idUsuario;
+   
+   @Column(unique = true, nullable = false)
    private String email;
-
+   
+   @Column(nullable = false)
    private String password;
-
+   
+   @Column(nullable = false)
    private String nome;
-
+   
+   @Column(unique = true, nullable = false, length = 11)
    private String cpf;
-
-   private LocalDate dt_dascimento;
-
-   private String fone_celular;
-
-
+   
+   @Column(name = "dt_nascimento") 
+   private LocalDate dtNascimento;  
+   
+   @Column(name = "fone_celular", length = 15) 
+   private String foneCelular;  
 }
