4e5a768d0c0aea47e279e8e0eb9bdd91db21b7f2
karlos <karlosmarques@gmail.com>
2025-11-23 10:02:01 -0300

    rota de login alterada

--- src/main/java/com/ifood/ifood_java/controller/auth/AuthController.java
+++ src/main/java/com/ifood/ifood_java/controller/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.ifood.ifood_java.controller.auth;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ifood.ifood_java.entity.usuario.LoginRequest;
+import com.ifood.ifood_java.entity.usuario.ResponseLoginRequest;
+import com.ifood.ifood_java.entity.usuario.Usuario;
 import com.ifood.ifood_java.entity.usuario.UsuarioRequest;
-
+import com.ifood.ifood_java.infra.security.TokenService;
+import com.ifood.ifood_java.repository.UsuarioRepository;
 import com.ifood.ifood_java.service.auth.AuthService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -28,9 +33,8 @@ public class AuthController {
         return ResponseEntity.ok(resultado);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest request){
-        String resultado = authServive.login(request);
-        return ResponseEntity.ok(resultado);
-    }
+@PostMapping("/login")
+public ResponseEntity<ResponseLoginRequest> login(@RequestBody LoginRequest request) {
+    return ResponseEntity.ok(authServive.login(request));
+}
 }
