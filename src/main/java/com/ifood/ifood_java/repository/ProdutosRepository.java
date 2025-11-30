7320122d2cc75d71e8c892af790431d8a4bd23ac
karlos <karlosmarques@gmail.com>
2025-11-30 17:39:35 -0300

    repository de  produto

---
+++ src/main/java/com/ifood/ifood_java/repository/ProdutosRepository.java
@@ -0,0 +1,17 @@
+package com.ifood.ifood_java.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ifood.ifood_java.entity.produtos.Produtos;
+import com.ifood.ifood_java.entity.restaurante.Restaurante;
+
+
+public interface ProdutosRepository extends JpaRepository<Produtos,Long> {
+    
+   List<Produtos> findAllByRestauranteIdRestaurante(Long idRestaurante);
+
+
+}
