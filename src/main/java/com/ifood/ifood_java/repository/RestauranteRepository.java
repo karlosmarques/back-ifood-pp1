ec283956d6f01f6a148e7347d4841a70511ab980
karlos <karlosmarques@gmail.com>
2025-11-30 17:39:53 -0300

    findByUusuario

--- src/main/java/com/ifood/ifood_java/repository/RestauranteRepository.java
+++ src/main/java/com/ifood/ifood_java/repository/RestauranteRepository.java
@@ -11,6 +11,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante,Long>{
 
 Optional<Restaurante> findById(Long idUsuario);
 List<Restaurante> findAllByUsuarioIdUsuario(Long idUsuario);
+Optional<Restaurante> findByUsuario(Usuario usuario);
 
 
    
