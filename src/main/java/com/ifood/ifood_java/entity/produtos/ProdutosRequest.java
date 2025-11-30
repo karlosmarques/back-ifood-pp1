d671f5b5aaaabde457719e5f1d96b38401c195b9
karlos <karlosmarques@gmail.com>
2025-11-30 17:38:17 -0300

    produtoRequest

---
+++ src/main/java/com/ifood/ifood_java/entity/produtos/ProdutosRequest.java
@@ -0,0 +1,28 @@
+package com.ifood.ifood_java.entity.produtos;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class ProdutosRequest {
+   @NotBlank(message = "O nome do produto é obrigatório.")
+    private String nome;
+
+    @NotBlank(message = "A descrição do produto é obrigatória.")
+    private String descricao;
+
+    @NotNull(message = "O preço é obrigatório.")
+    @Positive(message = "O preço deve ser maior que zero.")
+    private BigDecimal preco;
+
+    @NotNull(message = "O campo 'ativo' é obrigatório.")
+    private Boolean ativo;
+
+}
