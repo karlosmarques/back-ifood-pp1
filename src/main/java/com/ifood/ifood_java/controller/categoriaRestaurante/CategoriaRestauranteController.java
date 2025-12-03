0bc7dcae38c589cce28df4754d3fdeb94bd53f69
karlos <karlosmarques@gmail.com>
2025-12-03 10:07:21 -0300

    .

---
+++ src/main/java/com/ifood/ifood_java/controller/categoriaRestaurante/CategoriaRestauranteController.java
@@ -0,0 +1,38 @@
+package com.ifood.ifood_java.controller.categoriaRestaurante;
+
+import com.ifood.ifood_java.entity.categoria.CategoriaRestaurante;
+import com.ifood.ifood_java.service.categoria.CategoriaRestauranteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias/restaurantes")
+public class CategoriaRestauranteController {
+
+    private final CategoriaRestauranteService service;
+
+    public CategoriaRestauranteController(CategoriaRestauranteService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<CategoriaRestaurante> listar() {
+        return service.listarTodos();
+    }
+
+    @PostMapping
+    public CategoriaRestaurante salvar(@RequestBody CategoriaRestaurante categoria) {
+        return service.salvar(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public CategoriaRestaurante atualizar(@PathVariable Long id, @RequestBody CategoriaRestaurante categoria) {
+        return service.atualizar(id, categoria);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+}
