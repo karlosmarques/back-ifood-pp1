6dae691323b500fd71860005b5e67138e0b1f282
karlos <karlosmarques@gmail.com>
2025-12-03 15:12:30 -0300

    pedidoRepository

--- src/main/java/com/ifood/ifood_java/repository/PedidoRepository.java
+++ src/main/java/com/ifood/ifood_java/repository/PedidoRepository.java
@@ -1,8 +1,15 @@
 package com.ifood.ifood_java.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.ifood.ifood_java.modal.Pedido;
+
+import com.ifood.ifood_java.entity.pedido.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+   List<Pedido> findByClienteIdUsuario(Long idUsuario);
+
+
+    List<Pedido> findAllByRestauranteIdRestaurante(Long restauranteId);
 }
