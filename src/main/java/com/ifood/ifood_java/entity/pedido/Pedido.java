aca2efebd7f65e30931aa533c0d7c3d60bbab8f6
karlos <karlosmarques@gmail.com>
2025-12-03 15:11:29 -0300

    pedido entity

---
+++ src/main/java/com/ifood/ifood_java/entity/pedido/Pedido.java
@@ -0,0 +1,40 @@
+package com.ifood.ifood_java.entity.pedido;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.ifood.ifood_java.entity.restaurante.Restaurante;
+import com.ifood.ifood_java.entity.usuario.Usuario;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter 
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal valorTotal;
+
+    private LocalDateTime dataCriacao;
+
+    private String status; 
+    
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Usuario cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_restaurante")
+    private Restaurante restaurante;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<PedidoItem> itens;
+}
