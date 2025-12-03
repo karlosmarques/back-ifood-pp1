d41d36ac1b5251d3ac7b2f0af38951ffd0dc333f
karlos <karlosmarques@gmail.com>
2025-12-03 15:10:07 -0300

    pedido controller

---
+++ src/main/java/com/ifood/ifood_java/controller/pedido/PedidoController.java
@@ -0,0 +1,48 @@
+package com.ifood.ifood_java.controller.pedido;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import com.ifood.ifood_java.entity.pedido.*;
+import com.ifood.ifood_java.service.pedido.PedidoService;
+
+@RestController
+@RequestMapping("/pedidos")
+@CrossOrigin
+public class PedidoController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+
+    @PostMapping
+    public ResponseEntity<PedidoResponse> criarPedido(@RequestBody PedidoRequest request) {
+        return ResponseEntity.ok(pedidoService.criarPedido(request));
+    }
+
+
+    @GetMapping("/historico/cliente")
+    public ResponseEntity<?> historicoCliente() {
+
+        Long userId = Long.parseLong(
+            SecurityContextHolder.getContext().getAuthentication().getName()
+        );
+
+        return ResponseEntity.ok(pedidoService.historicoCliente(userId));
+    }
+
+
+    @GetMapping("/historico/restaurante")
+    public ResponseEntity<?> historicoRestaurante() {
+
+        Long userId = Long.parseLong(
+            SecurityContextHolder.getContext().getAuthentication().getName()
+        );
+
+        return ResponseEntity.ok(pedidoService.historicoRestaurante(userId));
+    }
+}
