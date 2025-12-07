b339236aa8a4433b2c0f21234e4e31cc0441b9e0
karlos <karlosmarques@gmail.com>
2025-12-07 10:38:12 -0300

    melhorias no retorno

--- src/main/java/com/ifood/ifood_java/entity/usuario/UsuarioRequest.java
+++ src/main/java/com/ifood/ifood_java/entity/usuario/UsuarioRequest.java
@@ -1,16 +1,19 @@
 package com.ifood.ifood_java.entity.usuario;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Past;
 import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
+import com.ifood.ifood_java.entity.endereco.EnderecoRequest;
 
 @Getter
 @Setter
@@ -26,15 +29,19 @@ public class UsuarioRequest {
     private String email;
 
     @NotBlank(message = "Senha é obrigatória")
-    @Size(min = 20, message = "Senha deve ter no mínimo 6 caracteres")
+    @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
     private String password;
 
     @NotBlank(message = "CPF é obrigatório")
     private String cpf;
+
+    @NotNull(message = "Data de nascimento é obrigatória")
+    @Past(message = "A data deve ser no passado")
     @DateTimeFormat(pattern = "dd-MM-yyyy")
-    @NotBlank(message = "data de nascimento é obrigatório")
     private LocalDate dt_nascimento;
 
     @NotBlank(message = "celular é obrigatório")    
     private String fone_celular;
+
+    private EnderecoRequest endereco;
 }
