9dda445cc69c5a9453a46c1d0392312340ef264e
karlos <karlosmarques@gmail.com>
2025-12-07 10:35:21 -0300

    deletar restaurante

--- src/main/java/com/ifood/ifood_java/controller/restaurante/RestauranteController.java
+++ src/main/java/com/ifood/ifood_java/controller/restaurante/RestauranteController.java
@@ -8,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ifood.ifood_java.entity.restaurante.Restaurante;
 import com.ifood.ifood_java.entity.restaurante.RestauranteRequest;
@@ -29,11 +33,12 @@ public class RestauranteController {
     @Autowired
     private RestauranteService restauranteService;
 
-   @PostMapping
-    public ResponseEntity<Restaurante> criarRestaurante(@Valid @RequestBody RestauranteRequest request){
-    Restaurante restaurante = restauranteService.criarRestaurante(request);
+@PostMapping(consumes = {"multipart/form-data"})
+public ResponseEntity<Restaurante> criarRestaurante(@RequestPart("dados") RestauranteRequest request,@RequestPart("imagem") MultipartFile imagem){
+    Restaurante restaurante = restauranteService.criarRestaurante(request, imagem);
     return ResponseEntity.ok(restaurante);
 }
+
 
 
     @GetMapping
@@ -41,6 +46,13 @@ public class RestauranteController {
     var resultado = restauranteService.mostrarRestaurante();
         return ResponseEntity.ok(resultado);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarRestaurante(@PathVariable Long id) {
+    restauranteService.deletarRestaurante(id);
+    return ResponseEntity.noContent().build();
+}
+
 
     //mobile
 
