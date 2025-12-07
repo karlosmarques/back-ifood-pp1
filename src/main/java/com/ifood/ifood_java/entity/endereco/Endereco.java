0069dfe288143cacca8d0f074dcc7230731b164e
karlos <karlosmarques@gmail.com>
2025-12-07 10:35:49 -0300

    entity de endereço

---
+++ src/main/java/com/ifood/ifood_java/entity/endereco/Endereco.java
@@ -0,0 +1,32 @@
+package com.ifood.ifood_java.entity.endereco;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "enderecos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+}
+
