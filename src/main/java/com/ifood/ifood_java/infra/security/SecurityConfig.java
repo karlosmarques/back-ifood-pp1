8967580c37306288d13a7f4b7d7ba6a905169a82
karlos <karlosmarques@gmail.com>
2025-12-12 10:42:44 -0300

    cors para todas as rotas

--- src/main/java/com/ifood/ifood_java/infra/security/SecurityConfig.java
+++ src/main/java/com/ifood/ifood_java/infra/security/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,24 +27,39 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/perfil/**").permitAll()
-                        .requestMatchers("/home").permitAll()
-                        .requestMatchers("/categorias/restaurantes").permitAll()
-                        .requestMatchers("/restaurante/**").permitAll()
-                        .requestMatchers("/produtos/**").permitAll()
-                        .requestMatchers("/pedidos/**").permitAll()
-                        .requestMatchers("/uploads/**").permitAll()
-                        
-                        
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/auth/**").permitAll()
+                    .requestMatchers("/perfil/**").permitAll()
+                    .requestMatchers("/home").permitAll()
+                    .requestMatchers("/categorias/restaurantes").permitAll()
+                    .requestMatchers("/restaurante/**").permitAll()
+                    .requestMatchers("/produtos/**").permitAll()
+                    .requestMatchers("/pedidos/**").permitAll()
+                    .requestMatchers("/uploads/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:8082","http://localhost:3000")); 
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 
     @Bean
