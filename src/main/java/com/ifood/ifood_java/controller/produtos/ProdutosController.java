16a70792f6a473635a49f38f9c7e494c98b645e8
karlos <karlosmarques@gmail.com>
2025-12-14 09:56:15 -0300

    deletar e editar

--- src/main/java/com/ifood/ifood_java/controller/produtos/ProdutosController.java
+++ src/main/java/com/ifood/ifood_java/controller/produtos/ProdutosController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -52,4 +55,16 @@ public ResponseEntity<Produtos> buscarProduto(@PathVariable Long produtoId) {
     return ResponseEntity.ok(produto);
 }
    
+@PutMapping("/editar/{id}")
+public ResponseEntity<Produtos>editarProduto(@RequestBody ProdutosRequest request, @PathVariable Long id) {
+    Produtos produtos = produtoService.editarProduto(request,id);
+    return ResponseEntity.ok(produtos);
+}
+
+@DeleteMapping("/deletar/{id}")
+public ResponseEntity<Void>deletarProduto(@PathVariable Long id) {
+    produtoService.deletarProduto(id);
+    return ResponseEntity.noContent().build();
+}
+
 }
